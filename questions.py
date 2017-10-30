4d1d878ca0b8085b0d85bfeb1bda9b02fcba3405
Felipe <felipefm@gmail.com>
2017-10-29 23:54:00 -0400

    Revert "Added q3() in questions.py"
    This reverts commit a14d8dc09b5dccd23dc3783e8da3acb95ad35a8c.

--- questions.py
+++ questions.py
@@ -9,12 +9,6 @@ def q1():
     global username
     username = input('What is your name?')
     Uservar.username = username
-def q4()
-    death = input ('Do you feel lucky punk?')
-    if death == yes:
-      print('DEATH')
-    else:
-      return q1()
 
 def pterodactyl:
     
