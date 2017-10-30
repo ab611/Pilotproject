f27067a02db81277a102afb9b476a69fcf44ca49
Austin <austin.bolton@gmail.com>
2017-10-30 00:20:13 -0400

    test3

--- questions.py
+++ questions.py
@@ -10,12 +10,3 @@ def q1():
     username = input('What is your name?')
     Uservar.username = username
 
-def pterodactyl:
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-    
->>>>>>> 68a5bc1a3c20ace9000756c740f88c56312d371d
-=======
-    
->>>>>>> bdda9606285d6203a7161196c7c95375770e8caa
